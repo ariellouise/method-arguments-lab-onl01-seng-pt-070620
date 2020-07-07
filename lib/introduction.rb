9d6b29136e45a01f4653045bb7157478f2128375
ariel murphy <arielladders@gmail.com>
2020-07-07 20:16:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-def introduction
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
@@ -6,4 +6,4 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end
 
-introduction
+introduction_with_language
